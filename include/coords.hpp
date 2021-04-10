1b18e409bcf544ec5f72fe806626c5546a2ef1e1
Seph De Busser <seph.debusser@gmail.com>
2021-04-10 13:11:02 +0200

    Added remaining coordinate conversions

--- include/coords.hpp
+++ include/coords.hpp
@@ -1,18 +1,25 @@
 #pragma once
 
+#include <SFML/System/Vector2.hpp>
+#include <SFML/System/Vector3.hpp>
 #include <array>
 
 struct TriCoord {
+	TriCoord(sf::Vector3i bary, int hex_size) : x(bary.x), y(bary.y), R(bary.x + bary.y + bary.z == hex_size * 3 - 2) {}
+	TriCoord(int x, int y, bool R) : x(x), y(y), R(R) {}
 	int x, y;
 	bool R;
+
+	std::array<TriCoord, 3> neighbors() {
+		int offset = R ? 1 : -1;
+		return { { {x,y,!R},{x + offset,y,!R},{x,y + offset,!R} } };
+	}
+
+	sf::Vector2f tri_center() {
+		return { x + (1 + R) / 3.f, y + (1 + R) / 3.f };
+	}
+
+	sf::Vector3i bary(int hex_size) {
+		return { x, y, hex_size * 3 - 1 - x - y - R };
+	}
 };
-
-std::array<TriCoord, 3> neighbors(TriCoord c) {
-	int offset = c.R ? 1 : -1;
-	return { { {c.x,c.y,!c.R},{c.x + offset,c.y,!c.R},{c.x,c.y + offset,!c.R} } };
-}
-
-template<typename Vec>
-Vec to_bary(TriCoord c) {
-	return Vec((c.x + 0.33f + c.R * 0.33f), (c.y + 0.33f + c.R * 0.33f));
-}
--- src/ExplodingTiles.cpp
+++ src/ExplodingTiles.cpp
@@ -1,6 +1,7 @@
 ﻿// ExplodingTiles.cpp : Defines the entry point for the application.
 //
 #include <string_view>
+#include <iostream>
 #include <cmath>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
@@ -8,11 +9,12 @@
 #include "coords.hpp"
 
 std::string_view shader = R"(
-const int hex_size = 5;
 const float edge_thickness = 0.06;
 const vec3 edge_color = vec3(1);
 const vec3 highlight_color = vec3(1,0,1);
-uniform vec2 mouse; //first two barycentric coordinates
+
+uniform int hex_size;
+uniform ivec3 selected;
 
 float min3(vec3 v) {
 	return min(min(v.x,v.y),v.z);
@@ -28,7 +30,6 @@ void main()
 	vec3 max_bound = vec3(hex_size*2+1);
 	vec3 bound_edge = vec3(edge_thickness*2);
 
-	ivec3 selected = floor(vec3(mouse,(1-mouse.x-mouse.y)) * (hex_size+1)*3);
 	vec3 coordinates = (gl_Color.rgb) * (hex_size+1) * 3;
 	ivec3 coords = floor(coordinates);
 	
@@ -74,14 +75,14 @@ int main()
 	t.append(sf::Vertex(sf::Vector2f(400 + size / 2, bottom), sf::Color::Green));
 	t.append(sf::Vertex(sf::Vector2f(400 - size / 2, bottom), sf::Color::Blue));
 
-	TriCoord c{ 5,5,false };
-
 	sf::Shader s;
 	{
 		sf::MemoryInputStream stream;
 		stream.open(shader.data(), shader.size());
 		s.loadFromStream(stream, sf::Shader::Type::Fragment);
 	}
+
+	s.setUniform("hex_size", hex_size);
 	sf::RenderStates render(&s);
 
 	// run the program as long as the window is open
@@ -92,26 +93,17 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
-			else if (event.type == sf::Event::KeyPressed) {
-				auto new_coords = neighbors(c);
-				if (event.key.code == sf::Keyboard::Num1)
-					c = new_coords[0];
-				else if (event.key.code == sf::Keyboard::Num2)
-					c = new_coords[1];
-				else if (event.key.code == sf::Keyboard::Num3)
-					c = new_coords[2];
-			}
 		}
 
 		window.clear(sf::Color::Black);
 
+		float v1 = 1 - dot(sf::Vector2f(sf::Mouse::getPosition(window)) - t[0].position, sf::Vector2f(0, 1)) / bary_length;
+		float v2 = 1 - dot(sf::Vector2f(sf::Mouse::getPosition(window)) - t[1].position, t[2].position + (t[0].position - t[2].position) / 2.f - t[1].position) / (bary_length * bary_length);
+		float v3 = 1 - v1 - v2;
 
+		TriCoord coord(sf::Vector3i(sf::Vector3f(v1,v2,v3)*((hex_size+1)*3.f)) - sf::Vector3i(1,1,1), hex_size);
 
-
-		//float v1 = 1 - dot(sf::Vector2f(sf::Mouse::getPosition(window)) - t[0].position, sf::Vector2f(0, 1)) / bary_length;
-		//float v2 = 1 - dot(sf::Vector2f(sf::Mouse::getPosition(window)) - t[1].position, t[2].position + (t[0].position - t[2].position) / 2.f - t[1].position) / (bary_length * bary_length);
-
-		s.setUniform("mouse", to_bary<sf::Vector2f>(c)/((hex_size+1)*3.f));
+		s.setUniform("selected", coord.bary(hex_size) + sf::Vector3i(1,1,1));
 		window.draw(t, render);
 
 		window.display();

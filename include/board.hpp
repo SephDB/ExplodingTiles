72eeb4f02455ea81256b61d1b014f0f759e6942d
Seph De Busser <seph.debusser@gmail.com>
2021-04-11 13:14:28 +0200

    Explosions and board update step!

--- include/board.hpp
+++ include/board.hpp
@@ -14,6 +14,11 @@ class Board {
 	std::vector<TileState> _state;
 	std::vector<TriCoord> _exploding;
 	int _size;
+
+	TileState& get(TriCoord c) {
+		return _state[c.x * 2 + c.y * _size * 4 + c.R];
+	}
+
 public:
 	Board() = default;
 	Board(int size) : _state(size* size * 8), _size(size) {}
@@ -45,18 +50,31 @@ public:
 		return _state[c.x * 2 + c.y * _size * 4 + c.R];
 	}
 
-	bool incTile(TriCoord c, int player) {
+	bool incTile(TriCoord c, int player, bool replace = false) {
 		if (!in_bounds(c))
 			return false;
 
-		TileState& s = _state[c.x * 2 + c.y * _size * 4 + c.R];
+		TileState& s = get(c);
 
-		if (s.player >= 0 && s.player != player) return false;
+		if (!replace && s.player >= 0 && s.player != player) return false;
 
 		s.player = player;
 		s.num++;
-		if (s.num > allowed_pieces(c)) _exploding = { c };
+		if (s.num > allowed_pieces(c)) _exploding.push_back(c);
 		return true;
 	}
 
+	void update_step() {
+		std::vector<TriCoord> old_exploding;
+		std::swap(old_exploding, _exploding);
+
+		for (auto& c : old_exploding) {
+			TileState& s = get(c);
+			if (s.num <= allowed_pieces(c)) continue;
+			for (auto& n : c.neighbors()) {
+				s.num -= incTile(n, s.player, true);
+			}
+			if (s.num == 0) s.player = -1;
+		}
+	}
 };
--- src/ExplodingTiles.cpp
+++ src/ExplodingTiles.cpp
@@ -72,6 +72,7 @@ class BoardView : public sf::Drawable {
 	const Board& b;
 
 	sf::CircleShape players[2];
+	sf::CircleShape explode;
 
 public:
 	TriCoord selected{};
@@ -107,15 +108,21 @@ public:
 		players[1] = players[0];
 		players[1].setFillColor(sf::Color::Red);
 		players[1].setPointCount(5);
+
+		explode = sf::CircleShape(size * 3);
+		explode.setFillColor(sf::Color::Yellow);
+		explode.setOutlineColor(sf::Color::Red);
+		explode.setOutlineThickness(-size / 2);
+		explode.setOrigin(size * 3, size * 3);
 	}
 
 	void updatePos(sf::Vector2f mouse) {
 		float length = inner[1].y - inner[0].y;
-		float v1 = 1 - dot(mouse - inner[0], sf::Vector2f(0,1)) / length;
+		float v1 = 1 - dot(mouse - inner[0], sf::Vector2f(0, 1)) / length;
 		float v2 = 1 - dot(mouse - inner[1], inner[2] + (inner[0] - inner[2]) / 2.f - inner[1]) / (length * length);
-		
-		sf::Vector3i bary = sf::Vector3i(3.f * b.size() * sf::Vector3f(v1, v2, 1-v1-v2));
-		
+
+		sf::Vector3i bary = sf::Vector3i(3.f * b.size() * sf::Vector3f(v1, v2, 1 - v1 - v2));
+
 		//ensure out of bounds coordinate when a coordinate < 0, converting to int != floor. Subtract one if a coordinate was below 0
 		bary -= sf::Vector3i(v1 < 0, v2 < 0, v1 + v2 > 1);
 
@@ -125,23 +132,29 @@ public:
 
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const override {
 		target.draw(outer, 3, sf::PrimitiveType::Triangles, { states.blendMode, states.transform, states.texture, &shader });
-		
+
 		auto draw_tile = [this, &target](TriCoord c, sf::RenderStates states) {
 			if (!b.in_bounds(c)) return;
 			auto s = b[c];
 			if (s.num == 0) return;
 
-			const sf::CircleShape& circle = players[s.player];
+			auto [x, y, z] = c.tri_center(b.size());
 			
-			auto [x,y,z] = c.tri_center(b.size());
+			states.transform.translate(x * inner[0] + y * inner[1] + z * inner[2]);
+			
+			if (s.num > b.allowed_pieces(c)) {
+				target.draw(explode, states);
+				return;
+			}
+
+			const sf::CircleShape& circle = players[s.player];
 
 			sf::Vector2f offset{};
 
 			if (s.num == 2 && c.R) {
-				offset = -circle.getOrigin() / 2.f;
+				states.transform.translate(-circle.getOrigin() / 2.f);
 			}
 
-			states.transform.translate(x*inner[0] + y*inner[1]+z*inner[2] + offset);
 			target.draw(circle, states);
 
 			if (s.num == 2) {
@@ -149,11 +162,11 @@ public:
 				target.draw(circle, states);
 			}
 		};
-		
+
 		for (int x = 0; x < b.size() * 2; ++x) {
 			for (int y = 0; y < b.size() * 2; ++y) {
-				draw_tile({ x,y,false },states);
-				draw_tile({ x,y,true },states);
+				draw_tile({ x,y,false }, states);
+				draw_tile({ x,y,true }, states);
 			}
 		}
 	}
@@ -182,7 +195,9 @@ int main()
 				window.close();
 				return 0;
 			case sf::Event::MouseButtonReleased:
-				if (b.incTile(board.selected, current_player)) {
+				if (b.needs_update()) {
+					b.update_step();
+				} else if (b.incTile(board.selected, current_player)) {
 					current_player = 1 - current_player; //switch between 0 and 1
 				}
 			}
